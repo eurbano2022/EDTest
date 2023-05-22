6fc24d3631c5984baf4f771d47f46b1626d956f7
cdtello <carlos.tello@correounivalle.edu.co>
2023-05-21 20:19:19 -0500

    config lambda Serverless Backend fix 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AssessmentsModule } from './assessments/assessments.module';
-import { ServicesService } from './users.services.ts/users/services/services.service';
 
 @Module({
     imports: [UsersModule, AssessmentsModule],
     controllers: [AppController],
-    providers: [AppService, ServicesService],
+    providers: [AppService],
 })
 export class AppModule {}
