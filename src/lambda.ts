0798cfb73ccc40a1059566c60dd50324cbf74b59
cdtello <carlos.tello@correounivalle.edu.co>
2023-06-12 20:13:25 -0500

    fix cors

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 import * as cors from 'cors';
+import * as express from 'express';
 
 import { AppModule } from './app.module';
 
@@ -10,35 +11,48 @@ let cachedServer;
 
 export const handler = async (event, context) => {
     if (!cachedServer) {
-        const nestApp = await NestFactory.create(AppModule);
+        const nestApp = await NestFactory.create(AppModule, {
+            cors: true,
+        });
+
+        nestApp.use(cors());
+
+        nestApp.use((req, res, next) => {
+            res.contentType('application/json');
+            next();
+        });
+
         nestApp.useGlobalPipes(
             new ValidationPipe({
                 whitelist: true,
                 forbidNonWhitelisted: true,
             }),
         );
+
         const config = new DocumentBuilder()
             .setTitle('ED Test')
             .setDescription('Management of the Maturity Assessment')
             .setVersion('1.0')
-            // .addApiKey({ type: 'apiKey', name: 'Authorization', in: 'header' }, 'bearerToken')
             .build();
+
         const document = SwaggerModule.createDocument(nestApp, config);
         SwaggerModule.setup('docs', nestApp, document);
+
         await nestApp.init();
+
         cachedServer = serverlessExpress({
             app: nestApp.getHttpAdapter().getInstance(),
         });
     }
 
     return cachedServer(event, context);
-
-    // // Agregar los encabezados en la respuesta
-    // response.headers = {
-    //     ...response.headers,
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Origin': '*',
-    // };
-
-    // return response;
 };
+
+// // Agregar los encabezados en la respuesta
+// response.headers = {
+//     ...response.headers,
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*',
+// };
+
+// return response;
