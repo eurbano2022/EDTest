ccf3889fe9772d308f06957ddf539d568d8b73b1
cdtello <carlos.tello@correounivalle.edu.co>
2023-07-09 15:02:47 -0500

    add new entities

--- src/assessments/assessments.module.ts
+++ src/assessments/assessments.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ActivitiesController } from './controllers/activities.controller';
-import { AnswersController } from './controllers/answers/answers.controller';
-import { CompaniesController } from './controllers/companies.controller';
+import { AnswersController } from './controllers/answers.controller';
 import { CriteriaController } from './controllers/criteria.controller';
-import { GoalsController } from './controllers/goals.controller';
 import { LevelsController } from './controllers/levels.controller';
-import { PracticesController } from './controllers/practices.controller';
 import { ProcessAreasController } from './controllers/process-areas.controller';
 import { ResourcesController } from './controllers/resources.controller';
 import { Activity } from './entities/activity.entity';
@@ -20,11 +17,6 @@ import { CriteriaService } from './services/criteria.service';
 import { LevelsService } from './services/levels.service';
 import { ProcessAreasService } from './services/process-areas.service';
 import { ResourcesService } from './services/resources.service';
-import { PracticesService } from './controllers/practices.service';
-import { PracticesService } from './services/practices.service';
-import { CompaniesService } from './services/companies.service';
-import { GoalsService } from './services/goals.service';
-import { AnswersService } from './services/answers.service';
 
 @Module({
     imports: [
@@ -42,10 +34,6 @@ import { AnswersService } from './services/answers.service';
         CriteriaService,
         ActivitiesService,
         ResourcesService,
-        PracticesService,
-        CompaniesService,
-        GoalsService,
-        AnswersService,
     ],
     controllers: [
         CriteriaController,
@@ -54,9 +42,6 @@ import { AnswersService } from './services/answers.service';
         ActivitiesController,
         ResourcesController,
         AnswersController,
-        GoalsController,
-        CompaniesController,
-        PracticesController,
     ],
 })
 export class AssessmentsModule {}
